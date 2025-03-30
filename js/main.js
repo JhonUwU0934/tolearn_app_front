4896f6bc1c50fb7832c56c698819b909f200169e
Naty004 <grjuliana7@gmail.com>
2025-03-30 00:55:38 -0500

    Visual inicial y registro e interacción con control
    Se termino de organizar la visual inicial y junto a está se realizo la visual de registro, para la interacción con las mismas también se acomodo la interacción del control remoto con el formulario y botones, quedaría faltando añadir la interacción con teclado por medio de la pantalla y las demás vistas.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,73 @@
+//Interacción del control con la vista
 
+document.addEventListener('DOMContentLoaded', function () {
+    const campos = document.querySelectorAll('.campo'); //Se seleccionan los campos y se guardan en un array
+    const botones = document.querySelectorAll('.boton'); //Lo mismo con los botones
+    const elementosInteractivos = Array.from(campos).concat(Array.from(botones)); // Se combinan los arrays campos y botones
+    let index = -1; // Se inicializa el índice
 
+    // Manejo de las teclas del control del televisor
+
+    document.addEventListener('keydown', function (event) { // Se utiliza 'event'para capturar la tecla presionada
+
+        if (elementosInteractivos && elementosInteractivos.length > 0) { //Se valida que el arreglo no este vacío
+            elementosInteractivos.forEach(function (elemento, i) { //Se utiliza una funcion estadar para iterar
+
+                if (elemento === document.activeElement) {
+                    index = i; //Se actualiza el índice inicial al del elemento enfocado
+                }
+            });
+
+            switch (event.keyCode) {
+                //Arriba
+                case 38:
+                    if (index > 0) {
+                        elementosInteractivos[index - 1].focus(); //Enfoca al elemento anterior
+                        console.log('Arriba');
+                    }
+                    break;
+
+                //Abajo
+                case 40:
+                    if (index < elementosInteractivos.length - 1) {
+                        elementosInteractivos[index + 1].focus(); //Enfoca al siguiente elemento
+                        console.log('Abajo');
+                    }
+                    break;
+
+                //Click
+                case 13:
+                    if (index !== -1 && elementosInteractivos[index]) { //Se verifica la validez del indice y el elemento
+                        if (elementosInteractivos[index].classList.contains('boton')) {
+                            elementosInteractivos[index].click();
+                            console.log('Click botón:', elementosInteractivos[index].textContent || 'Sin texto');
+                        } else {
+                            console.log('Click en el campo:', elementosInteractivos[index].id || 'Elemento no encontrado');
+                        }
+                    } else {
+                        console.warn('No hay elemento interactivo enfocado para darle click'); //Advertencia
+                    }
+                    break;
+
+                default:
+                    console.log('Boton no reconocido:', event.keyCode);
+                    break;
+            }
+        } else {
+            console.warn('El arreglo "elementosInteractivos" esta vacio o no existe.'); //Advertencia
+        }
+    });
+
+    //Redireccion desde inicio.html hacia registro.html por medio del btnComenzar
+
+    const btnComenzar = document.getElementById('btnComenzar'); //Selecciona el botón por su ID
+    if (btnComenzar) {
+        btnComenzar.addEventListener('click', function () {
+            console.log('Click comenzar');
+            window.location.href = 'registro.html'; //Se redirecciona a registro.html
+        });
+    }
+    else {
+        console.error('El boton no se encuentra.'); //Error
+    }
+});
